05ce76ce43e35ca0b67b5b92d7af45e8c97729e1
thankforcat <ffslmo.wmy@gmail.com>
2023-03-23 22:08:32 +0800

    1.1

--- static/js/index.js
+++ static/js/index.js
@@ -3,36 +3,57 @@
 //3.点击 checkbox 就会出现删除线
 //4.并且点击过 checkbox 的 div 会消失
 //5.鼠标悬停会有阴影. CSS 去实现
-
-
-
-
-
-
-
-
-
-function addTodo(){
+document.addEventListener('DOMContentLoaded',function () {
+  const addDiv = document.getElementById('hahaha')
+  addDiv.addEventListener('click',function () {
     const newBox = document.createElement("div")
     const boxlist = document.getElementById("boxlist")
     const checkbox = document.createElement("input")
     const span = document.createElement("span")
     const rectangle = document.createElement("div")
-
-    for (let i = 0; i < 8; i++){
-      newBox.classList.add("box")
-      checkbox.type = "checkbox"
-      checkbox.setAttribute("id","checkbox"+i)
-      span.textContent = "这里请输入内容"
-      rectangle.classList.add("rectangle")
-4 
-    }
-
-
-
+    
+    newBox.classList.add("box")
+    checkbox.type = "checkbox"
+    checkbox.setAttribute("id","checkbox")
+    span.textContent = "这里请输入内容"
+    rectangle.classList.add("rectangle")
+    
     newBox.appendChild(checkbox)
     newBox.appendChild(span)
     newBox.appendChild(rectangle)
     boxlist.appendChild(newBox)
-}
+})})
 
+
+// 写了N总方法都错了几乎放弃
+// document.addEventListener('DOMContentLoaded',function () {
+//   const checkboxes = document.getElementById('checkbox')
+//   const spanText = document.querySelector('.text')
+
+// checkboxes.addEventListener('click',function () {
+//   // spanText.style.textdecoration = `"line-through"`
+//   if (checkboxes == 'checked') {
+//     spanText.style.textdecoration = 'none';
+//   }else{
+//     spanText.style.textdecoration = 'line-through'
+//   }
+// })
+// })
+
+
+
+const spanText = document.getElementById('text')
+console.log(spanText); //找到的是null
+
+
+function checkboxOnclick(checkbox) {
+  if (checkbox.checked == true) {
+    console.log('123');
+  spanText.style.textdecoration = 'none';
+    
+  }else{
+    console.log('hahaha');
+  spanText.style.textdecoration = 'line-through'
+
+  }
+}
